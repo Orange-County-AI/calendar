0ab8b662a3cb7f27ee7ecce255c1f7dcdc1b673b
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2025-06-10 20:16:14 -0700

    Remove eslint-disable comments from type definitions in worker-configuration.d.ts for cleaner code.

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -3799,7 +3799,7 @@ type AIGatewayHeaders = {
     [key: string]: string | number | boolean | object;
 };
 type AIGatewayUniversalRequest = {
-    provider: AIGatewayProviders | string; // eslint-disable-line
+    provider: AIGatewayProviders | string;  
     endpoint: string;
     headers: Partial<AIGatewayHeaders>;
     query: unknown;
@@ -3815,7 +3815,7 @@ declare abstract class AiGateway {
         gateway?: GatewayOptions;
         extraHeaders?: object;
     }): Promise<Response>;
-    getUrl(provider?: AIGatewayProviders | string): Promise<string>; // eslint-disable-line
+    getUrl(provider?: AIGatewayProviders | string): Promise<string>;  
 }
 interface AutoRAGInternalError extends Error {
 }
